8518581160866bc215714398d8ef9f6945bda592
Sebastian Calderón <calderon_2412@hotmail.com>
2021-07-11 21:20:36 -0500

    Add files via upload

---
+++ comandos_git.sh
@@ -0,0 +1,58 @@
+# https://git-scm.com/book/en/v2
+# https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow
+# https://danielkummer.github.io/git-flow-cheatsheet/
+
+# Configuracion del entorno GIT
+shift + Ins                         #para pegar codigo en terminal Git bash (Insert)
+git init                            #crea repositorio
+git config                          #todas las configuraciones de Git
+git config --list                   #configuracion actual de Git
+git config --list --show-origin     #origen del archivo gitconfig(avanzado)
+git config --global user.name "Sebastian Calderon"
+git config --global user.email "calderon950527@gmail.com"
+
+
+# Primeros pasos
+git add archivo.xxx                 #añade archivo al Staging Area (ram)
+git restore archivo.xxx             #discard changes in working directory
+git restore --staged archivo.xxx    #unstage. back to working directory
+git commit -m " "                   #envia el archivo al repositorio
+git commit -am " "                  #-a saltarse el add;
+git status                          #estado de los archivos
+git status -s (--short)             #estado conciso de los archivos
+                                    #left-staged; right-unstaged
+                                    #??-untracked; A-staged(nuevo); M-modified
+
+
+# Commits y cambios realizados
+git show archivo.xxx                #historial de cambios del archivo (git show)
+git log archivo.xxx                 #historial del archivo (git log)
+git log --stat                      #archivos en que se hicieron cambios por commit
+git diff old_code new_code          #comparar versiones de commit
+git diff                            #Working directory vs Repositorio
+git diff --staged (--cached)        #staging Area vs Repositorio
+
+
+# Volver en el tiempo
+git reset old_code --soft           #version anterior pero manteniendo el staging
+git reset old_code --hard           #version anterior borrando todo los demás commits
+git checkout old_code archivo.xxx   #puedo revisar el archivo en su commit especificado
+git checkout master archivo.xxx     #vuelo a tener el archivo del ultimo commit
+
+
+# Traer cambios del repositorio
+checkout                            #
+
+
+# Branch
+
+
+# Merge
+
+
+# Clone
+git clone https://github.com/calderon9505/DataScience_Platzi.git
+
+
+# Ignore
+cat .gitignore                      #lista de archivos o patrones a considerar (*.ini)
