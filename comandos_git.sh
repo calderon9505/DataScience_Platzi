482c7e63be8e2c6bcd464fdc4cc129e0f8bc1314
Sebastian Calderón <calderon950527@gmail.com>
2021-07-12 17:19:20 -0500

    Adicion de comandos en _Commits y cambios realizados_ esto sirvio para hacer la prueba de --amend

--- comandos_git.sh
+++ comandos_git.sh
@@ -1,18 +1,20 @@
 # https://git-scm.com/book/en/v2
 # https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow
 # https://danielkummer.github.io/git-flow-cheatsheet/
-
-# Configuracion del entorno GIT
 shift + Ins                         #para pegar codigo en terminal Git bash (Insert)
+
+
+# GIT Configuration
 git init                            #crea repositorio
 git config                          #todas las configuraciones de Git
 git config --list                   #configuracion actual de Git
 git config --list --show-origin     #origen del archivo gitconfig(avanzado)
 git config --global user.name "Sebastian Calderon"
 git config --global user.email "calderon950527@gmail.com"
+git config --global core.editor "code --wait"
 
 
-# Primeros pasos
+# First steps
 git add archivo.xxx                 #añade archivo al Staging Area (ram)
 git restore archivo.xxx             #discard changes in working directory
 git restore --staged archivo.xxx    #unstage. back to working directory
@@ -24,18 +26,26 @@ git status -s (--short)             #estado conciso de los archivos
                                     #??-untracked; A-staged(nuevo); M-modified
 
 
-# Commits y cambios realizados
+# Commit history and comparations
 git show archivo.xxx                #historial de cambios del archivo (git show)
 git log archivo.xxx                 #historial del archivo (git log)
+git log -p -2                       #-p diferencias introduc. -2 solo dos commits
 git log --stat                      #archivos en que se hicieron cambios por commit
+git log --pretty=oneline            #cada commit es una linea (short, full, fuller)
+git log --graph                     #grafico ASCII muestra historial (usar con oneline)
+git log --since=" "                 #(2.weeks)("2008-01-15")("2 years 1 day 3 minutes ago")
+git log --author " " --grep " " --all-match
 git diff old_code new_code          #comparar versiones de commit
 git diff                            #Working directory vs Repositorio
 git diff --staged (--cached)        #staging Area vs Repositorio
 
 
-# Volver en el tiempo
+# Undoing Things
+git commit --amend                  
 git reset old_code --soft           #version anterior pero manteniendo el staging
 git reset old_code --hard           #version anterior borrando todo los demás commits
+
+
 git checkout old_code archivo.xxx   #puedo revisar el archivo en su commit especificado
 git checkout master archivo.xxx     #vuelo a tener el archivo del ultimo commit
 
