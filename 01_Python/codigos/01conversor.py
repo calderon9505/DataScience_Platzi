e3eea3ddb96d1ef3af0fd440fffa4cc0ff1a726e
Sebastian Calderon <calderon950527@gmail.com>
2021-07-22 22:54:33 -0500

    traslado de imagenes y arreglos al curso de python 1

--- 01_Python/codigos/01conversor.py
+++ 01_Python/codigos/01conversor.py
@@ -2,30 +2,19 @@
 # print("Tienes $" + str(round(pesos/3875 , 2)) + " dolares")
 # print("Tienes $", round(pesos/3875 , 2), " dolares")
 
-
 # if pesos > 1000:
 #     pass
 # else:
 #     pass
 
-# wndw + . para emojis
+# >>> type(True)    <class 'bool'>
+# >>> type("hola")  <class 'str'>
+# >>> type(3.2)     <class 'float'>
+# >>> type((5,2))   <class 'tuple'>
+# >>> type([5,2])   <class 'list'>
 
-# >>> type(True)
-# <class 'bool'>
-# >>> type("hola")
-# <class 'str'>
-# >>> type(3.2)
-# <class 'float'>
-# >>> type((5,2))
-# <class 'tuple'>
-# >>> type([5,2])
-# <class 'list'>
-# >>>
-
-# >>> 9//2
-# 4
-# >>> 9**2
-# 81
+# >>> 9//2          4 Cociente
+# >>> 9**2          81 Potencia
 
 # '''texto(string) en
 # varias lineas'''
@@ -33,13 +22,12 @@
 # def funcion_prueba(x):
 #     print(f"funcion de prueba {x}")
 # funcion_prueba("texto 1")
-# funcion_prueba(2)
-# funcion_prueba(3)
+# funcion_prueba(2948)
 
 nombre = " sebastiam " #espacios basura
-# print(nombre.upper())
 nombre = nombre.strip().capitalize().replace("m","n") #orden correcto
-print(nombre)
-print(len(nombre))
+# upper()            convertir a mayusculas
+# strip()            elimina espacios iniciales y finales
+print(nombre + "\t" + str(len(nombre)))
 print(nombre[::-1])
 
