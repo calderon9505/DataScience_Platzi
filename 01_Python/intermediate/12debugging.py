4f01aa7382ba6e4b28c888b92cb1e4a412bc4560
Sebastian Calderon <calderon950527@gmail.com>
2021-08-04 22:28:57 -0500

    Exceptions

--- 01_Python/intermediate/12debugging.py
+++
@@ -1,9 +0,0 @@
-def divisor(num):
-    return [i for i in range (1,num+1) if num%i == 0]
-
-def run():
-    num = int(input('ingrese número:  '))
-    print(divisor(num))
-
-if __name__ == '__main__':
-    run()
