8e28f04de46a1c5a2c4097975b130b3ab25facc2
Sebastian Calderon <calderon950527@gmail.com>
2022-01-27 22:57:20 -0500

    Visualización de datos de pandas en el ETL

--- 09_DataEngineer/ETL/Transform/newspaper_receipe.py
+++ 09_DataEngineer/ETL/Transform/newspaper_receipe.py
@@ -3,33 +3,41 @@ import logging
 from urllib.parse import urlparse
 import pandas as pd
 import hashlib
+import nltk
+from nltk.corpus import stopwords
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def main(filename):
+def main(path):
     logger.info('Starting Data Wragling')
-    df = _read_data(filename)
-    _add_newspaper_uid_column(df, filename)
-    _add_host_column(df)
+    df = _read_data(path)
+    _remove_duplicates(df, 'title')
     _fill_miising_titles(df)
-    _generate_uids_index(df)
+    _drop_rows_with_missing_values(df)
     _remove_new_lines_from_body(df)
+    _add_newspaper_uid_column(df, path)
+    _add_host_column(df)
+    _generate_uids_index(df)
+    _add_tokenize_column(df, 'title')
+    _add_tokenize_column(df, 'body')
+    _save_data(df, path)
     return df
 
-def _read_data(filename):
-    logger.info(f'Reading {filename}')
-    return pd.read_csv(filename)
+def _read_data(path):
+    logger.info(f'Reading {path}')
+    return pd.read_csv(path)
 
-def _add_newspaper_uid_column(df, filename):
-    newspaper_uid = filename.split('\\')[2].split('_')[0]
+def _add_newspaper_uid_column(df, path):
+    # No sirve para cualquier Path
+    newspaper_uid = path.split('\\')[2].split('_')[0]
     df['newspaper_uid'] = newspaper_uid
     logger.info(f'newspaper uid column filled: {newspaper_uid}')
 
 def _add_host_column(df):
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
-    logger.info(f'host column filled')
+    logger.info('host column filled')
 
 def _fill_miising_titles(df):
     missing_titles_mask = df['title'].isna()
@@ -39,7 +47,7 @@ def _fill_miising_titles(df):
         .str.replace('-',' ').str.capitalize()
     )
     df.loc[missing_titles_mask, 'title'] = missing_titles
-    logger.info(f'missing titles filled')
+    logger.info('missing titles filled')
 
 def _generate_uids_index(df):
     uids = (
@@ -50,17 +58,50 @@ def _generate_uids_index(df):
     )
     df['uid'] = uids
     df.set_index('uid', inplace=True)
-    logger.info(f'hash index generated')
+    logger.info('hash index generated')
 
 def _remove_new_lines_from_body(df):
     df['body'] = df['body'].str.replace('\n','')
-    logger.info(f'new lines removed from body')
+    logger.info('new lines removed from body')
 
+def _add_tokenize_column(df, column):
+    stop_words = stopwords.words('Spanish')
+    df['n_tokens_'+column] = (
+        df[column].dropna()
+        .apply(lambda row: nltk.word_tokenize(row))
+        .apply(lambda words: list(filter(lambda word: word.isalpha(), words)))
+        .apply(lambda words: list(map(lambda word: word.lower(), words)))
+        .apply(lambda words: list(filter(lambda word: word not in stop_words, words)))
+        .apply(lambda words: len(words))
+    )
+    logger.info('tokenize column filled')
+
+def _remove_duplicates(df, column):
+    df.drop_duplicates(subset=[column], inplace=True)
+    logger.info(f'duplicates removed based on: {column}')
+
+def _drop_rows_with_missing_values(df):
+    df.dropna()
+    logger.info('rows with missing values dropped')
+
+def _save_data(df, path):
+    filename = path.split('\\')[2]
+    filename = f'.\Transform\clean_{filename}'
+    # df.to_csv(filename)
+    # La codificación es importante para excel
+    # aunque se puede ajustar desde excel
+    df.to_csv(filename, encoding='utf-8-sig')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help='The path to the dirty data', type=str)
+    parser.add_argument('path', help='The path to the dirty data', type=str)
     args = parser.parse_args()
-    print(args.filename)
-    df = main(args.filename)
-    print(df[['newspaper_uid','host']])
+    df = main(args.path)
+    print(df.loc[:,'newspaper_uid':])
+
+
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+# cd .\09_DataEngineer\ETL\
+# .\venv\Scripts\activate
+# py .\Transform\newspaper_receipe.py .\Extract\elpais_2022_01_25.csv
+# py .\Transform\newspaper_receipe.py .\Extract\eluniversal_2022_01_25.csv
